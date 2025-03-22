5abebf7c5aa486b15dc37dd02e3ec6576b31882c
Alexander Ikonomou <alexander.ikonomou.19@proton.me>
2025-03-22 13:43:19 +0100

    Change names in Package.Swift and rename files
    I used the same cases as in
    https://github.com/swiftlang/swift-testing/blob/main/Package.swift.

--- Package.swift
+++ Package.swift
@@ -1,5 +1,4 @@
 // swift-tools-version: 6.0
-// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
@@ -9,19 +8,16 @@ let package = Package(
       .macOS(.v13),
     ],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
-            name: "swift-ansi-escapes",
-            targets: ["swift-ansi-escapes"]),
+            name: "AnsiEscapes",
+            targets: ["AnsiEscapes"]),
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "swift-ansi-escapes"),
+            name: "AnsiEscapes"),
         .testTarget(
-            name: "swift-ansi-escapesTests",
-            dependencies: ["swift-ansi-escapes"]
+            name: "AnsiEscapesTests",
+            dependencies: ["AnsiEscapes"]
         ),
     ]
 )
