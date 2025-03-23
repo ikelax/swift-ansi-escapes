306836d49f57057aa5308234b22236616590d7ad
Alexander Ikonomou <alexander.ikonomou.19@proton.me>
2025-03-23 10:05:55 +0100

    Improve description of MoveCursorDirections

--- Examples/MoveCursorDirections/main.swift
+++ Examples/MoveCursorDirections/main.swift
@@ -21,6 +21,14 @@ print("x", terminator: "")
 print(moveCursorDown(for: 6), terminator: "")
 // We moved 5 forward and printed 2 characters.
 // So, we have to move 7 backward.
+// We have to move backward first because moving
+// the cursor backward also deletes printed characters.
+// You can test this out by replacing `moveCursorForward`
+// with `moveCursorBackward` and deleting the
+// last print statement.
+// Thus, we first print the bottom left x
+// and after that move the cursor forward
+// and print the bottom right x.
 print(moveCursorBackward(for: 7), terminator: "")
 print("x", terminator: "")
 print(moveCursorForward(for: 5), terminator: "")
