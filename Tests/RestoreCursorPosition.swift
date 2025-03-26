2bf8a57f06e125d6546f5d7c3c829da3989d070d
ikelax <163678144+ikelax@users.noreply.github.com>
2025-03-26 19:28:57 +0100

    Remove isAppleTerminal and macros for os (#7)
    I removed the macros that make code only available on specific OSes.
    swift-dotenv uses Foundations' ProcessInfo.processInfo.environment on
    Linux [0]. Additionally, this makes it possible to generate ANSI escape
    codes for Windows while being on macOS, for example.
    
    By setting the default value of terminal as the environment variable
    TERM_PROGRAM, it is possible to test the method without having to mock
    ProcessInfo.
    
    [0]: https://github.com/thebarndog/swift-dotenv/tree/HEAD@{2025-03-26T18:10:27Z}/Sources/Dotenv.swift
    
    See pull request: https://github.com/ikelax/swift-ansi-escapes/pull/7

---
+++ Tests/RestoreCursorPosition.swift
@@ -0,0 +1,12 @@
+import Testing
+import AnsiEscapes
+
+@Suite("Restores the saved cursor position") struct RestoreCursorPosition {
+  @Test("in Apple's Terminal") func inAppleTerminal() {
+    #expect(ANSIEscapeCode.restoreCursorPosition(inTerminal: "Apple_Terminal") == "\u{001B}8")
+  }
+    
+  @Test("in other terminals") func otherTerminals() {
+    #expect(ANSIEscapeCode.restoreCursorPosition(inTerminal: "iTerm2") == "\(ANSIEscapeCode.ESC)u")
+  }
+}
