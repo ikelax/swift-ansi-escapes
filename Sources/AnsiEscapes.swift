12f9f8292ed6b5a0fcbbfe32b3543383b9a5b199
Alexander Ikonomou <alexander.ikonomou.19@proton.me>
2025-03-29 10:30:30 +0100

    Add comment to eraseLines

--- Sources/AnsiEscapes.swift
+++ Sources/AnsiEscapes.swift
@@ -178,6 +178,10 @@ public struct ANSIEscapeCode {
     ].joined(separator: "")
   }
   
+  /// Erase from the current cursor position up the specified amount of lines.
+  /// The cursor is moved to the left after the erasion.
+  /// - Parameter count: The number of lines which are erased.
+  /// - Returns: The ANSI escape code.
   public static func eraseLines(count: Int) -> String {
     var escapeCode = ""
     
